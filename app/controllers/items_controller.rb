08dc38c9841f714f95a76ea05112b42aa8962932
Oscar Wyatt <oscar.wyatt@digital.cabinet-office.gov.uk>
2019-08-28 15:34:03 +0100

    Appease linter

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,8 +6,8 @@ class ItemsController < ApplicationController
     session[:uuid] = SecureRandom.uuid
     collection_data = Rails.application.config_for(:items)[params[:items]]
     if collection_data
-      @items = CSV.parse(File.read("data/#{collection_data["item_file"]}"), headers: true).map { |row| row }.shuffle
-      @answers = CSV.parse(File.read("data/#{collection_data["answers_file"]}"), headers: true)
+      @items = CSV.parse(File.read("data/#{collection_data['item_file']}"), headers: true).map { |row| row }.shuffle
+      @answers = CSV.parse(File.read("data/#{collection_data['answers_file']}"), headers: true)
       @collection = params[:items]
     else
       render "not_found"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "/:items" => "items#index"
   resources :answers, only: [:create]
-  get "/healthcheck", to: proc { [200, {}, ["OK"]] }
+  get "/healthcheck", to: proc { [200, {}, %w(OK)] }
 end
