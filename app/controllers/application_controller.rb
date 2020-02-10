5569dbd6ac6a82d10b4fbf184832dfb1e0a4844a
Oscar Wyatt <oscar.wyatt@digital.cabinet-office.gov.uk>
2020-02-10 17:33:09 +0000

    Add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV["GOVUK_USERNAME"], password: ENV["GOVUK_PASSWORD"], if: -> { Rails.env == "production" }
 
 private
 
